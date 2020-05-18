3127410935d6fc79cd7b1007f7b65eb59b66432f
d7p4n4 <bercikezs199999999@gmail.com>
2020-05-18 15:54:10 +0200

    Add project files.

---
+++ CSAc4yRestServiceClientCore/Program.cs
@@ -0,0 +1,26 @@
+﻿using log4net;
+using log4net.Config;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace CSAc4yRestServiceClientCore
+{
+    class Program
+    {
+
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+        }
+    }
+}
